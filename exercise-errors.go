e0693562a7013d0149169072ca2e8646e374a15b
Davi Garcia (davivcgarcia) <davivcgarcia@gmail.com>
2018-02-01 19:02:22 -0200

    Methods and interfaces > Methods and Interfaces > Exercise: Errors

---
+++ exercise-errors.go
@@ -0,0 +1,46 @@
+/*
+Copyright 2017 Davi Garcia (davivcgarcia@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+
+	z := float64(x / 2)
+
+	for z*z > x*(1+1e-15) {
+		z -= (z*z - x) / (2 * z)
+	}
+
+	return z, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(3))
+	fmt.Println(Sqrt(-4))
+}
