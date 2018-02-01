7bac8bd9f9a0ed4f66980293b73cca69c524873a
Davi Garcia (davivcgarcia) <davivcgarcia@gmail.com>
2018-02-01 19:02:34 -0200

    Methods and interfaces > Methods and Interfaces > Exercise: Readers

---
+++ exercise-readers.go
@@ -0,0 +1,32 @@
+/*
+Copyright 2017 Davi Garcia (davivcgarcia@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+func (myReader MyReader) Read(b []byte) (int, error) {
+	for i := range b {
+		b[i] = 'A'
+	}
+	return len(b), nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
